e4953bef9e6f6b335ed1e0457af18fb36bc2d451
weslleymberg <weslleym.lisboa@gmail.com>
2014-09-03 14:03:35 -0300

    Changes the way that ratings data are returned

--- content_recommendation/content_recommendation.py
+++ content_recommendation/content_recommendation.py
@@ -1,7 +1,11 @@
 def load_ratings(file_):
-    result = {}
+    users = set()
+    movies = set()
+    ratings = {}
     with open(file_) as f:
         for line in f:
-            user_id, movie_id, rating, _ = line.split("::")
-            result[(user_id, movie_id)] = int(rating)
-    return result
+            user_id, movie_id, rating, _ = line.strip().split("::")
+            users.add(user_id)
+            movies.add(movie_id)
+            ratings[(user_id, movie_id)] = int(rating)
+    return (users, movies, ratings)
--- content_recommendation/content_recommendation_spec.py
+++ content_recommendation/content_recommendation_spec.py
@@ -15,12 +15,17 @@ class ContentRecommendationTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.ratings_file = generate_temporary_file("1::1193::5::978300760")
+        cls.ratings_file = generate_temporary_file("1::1193::5::978300760\n \
+                                                    2::1357::5::978298709\n \
+                                                    3::3421::4::978298147\n \
+                                                    12::1193::4::978220179")
 
     @classmethod
     def tearDownClass(cls):
         os.unlink(cls.ratings_file.name)
 
     def it_should_load_user_ratings(self):
-        result = load_ratings(self.ratings_file.name)
-        result |should| equal_to({('1', '1193'): 5})
+        users, movies, ratings = load_ratings(self.ratings_file.name)
+        users |should| equal_to({'1', '2', '3', '12'})
+        movies |should| equal_to({'1193', '1357', '3421'})
+        ratings |should| equal_to({('1','1193'): 5, ('2', '1357'): 5, ('3', '3421'): 4, ('12', '1193'): 4})
