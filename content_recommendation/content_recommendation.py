d31c1d68ddfde039266a683723748948ad39cdf9
Weslleymberg <weslleym.lisboa@gmail.com>
2014-09-03 20:28:57 -0300

    Return more similar users

--- content_recommendation/content_recommendation.py
+++ content_recommendation/content_recommendation.py
@@ -1,28 +1,69 @@
-from itertools import product
+from itertools import product, combinations
 from collections import defaultdict
+from math import sqrt
 
-def load_ratings(file_):
-    users = set()
-    movies = set()
-    ratings = {}
-    with open(file_) as f:
+def load_from_file(file_name):
+    with open(file_name) as f:
         for line in f:
-            user_id, movie_id, rating, _ = line.strip().split("::")
-            users.add(user_id)
-            movies.add(movie_id)
-            ratings[(user_id, movie_id)] = int(rating)
-    return (users, movies, ratings)
+            yield line
 
-def normalize_data(users, movies, ratings_table):
-    scores = defaultdict(lambda: 0)
-    normalized_table = {}
-    for i in product(users, movies):
-        u_id, m_id = i
-        try:
-            scores[u_id] += ratings_table[i]
-        except KeyError:
-            scores[u_id] += 0
-    for key, value in ratings_table.iteritems():
-        user_id, _ = key
-        normalized_table[key] = round(ratings_table[key] - (float(scores[user_id])/len(movies)), 3)
-    return normalized_table
+def sim(a, b):
+    def multiply(x, y):
+        return x*y
+    def sum_squared(y):
+        return sum(map(lambda x: x*x, y))
+    vd = sum(map(multiply, a, b))
+    vdd = sqrt(sum_squared(a))*sqrt(sum_squared(b))
+    return vd/vdd
+
+
+class Similar(object):
+    def __init__(self):
+        self.users = set()
+        self.movies = set()
+        self.ratings = {}
+        self.utility_matrix = {}
+
+    def load_ratings(self, file_name):
+        for line in load_from_file(file_name):
+            user_id, movie_id, rating, _ = line.strip().split("::")
+            self.users.add(user_id)
+            self.movies.add(movie_id)
+            self.ratings[(user_id, movie_id)] = int(rating)
+
+    def normalize_data(self):
+        scores = defaultdict(lambda: 0)
+        for i in product(self.users, self.movies):
+            u_id, m_id = i
+            try:
+                scores[u_id] += self.ratings[i]
+            except KeyError:
+                scores[u_id] += 0
+        for key, value in self.ratings.iteritems():
+            user_id, _ = key
+            self.utility_matrix[key] = round(value - (float(scores[user_id])/len(self.movies)), 3)
+
+    def _get_ratings(self, user_id):
+        user_ratings = []
+        for i in product((user_id,), self.movies):
+            try:
+                rate = self.utility_matrix[i]
+            except KeyError:
+                rate = 0
+            user_ratings.append(rate)
+        return user_ratings
+
+    def get_k_more_similar_users(self, k):
+        similars = {}
+        more_similars = set()
+        for a, b in combinations(self.users, 2):
+            ratings_a = self._get_ratings(a)
+            ratings_b = self._get_ratings(b)
+            similars[(a, b)] = sim(ratings_a, ratings_b)
+        sorted_similars = sorted(similars.iteritems(), key=lambda x: x[1], reverse=True)
+        for key, _ in sorted_similars:
+            for i in key:
+                if len(more_similars) == k:
+                    break
+                more_similars.add(i)
+        return list(more_similars)
--- content_recommendation/content_recommendation_spec.py
+++ content_recommendation/content_recommendation_spec.py
@@ -3,13 +3,23 @@ import os
 from tempfile import NamedTemporaryFile
 from should_dsl import should
 
-from content_recommendation import load_ratings, normalize_data
+from content_recommendation import Similar, sim
 
 def generate_temporary_file(data):
     temp_file = NamedTemporaryFile(delete=False)
     temp_file.write(data)
     temp_file.close()
     return temp_file
+
+
+class CosseneSimilarityTest(unittest.TestCase):
+
+    def it_should_return_the_similarity_between_two_sets(self):
+        result = sim([5,0,0], [0,5,0])
+        result |should| equal_to(0)
+        result = sim([5,0,0], [4,0,0])
+        result |should| equal_to(1)
+
 
 class ContentRecommendationTest(unittest.TestCase):
 
@@ -24,18 +34,37 @@ class ContentRecommendationTest(unittest.TestCase):
     def tearDownClass(cls):
         os.unlink(cls.ratings_file.name)
 
-    def it_should_load_user_ratings(self):
-        users, movies, ratings = load_ratings(self.ratings_file.name)
-        users |should| equal_to({'1', '2', '3', '12'})
-        movies |should| equal_to({'1193', '1357', '3421'})
-        ratings |should| equal_to({('1','1193'): 5, ('2', '1357'): 5, ('3', '3421'): 4, ('12', '1193'): 4})
+    def setUp(self):
+        self.similar = Similar()
+        self.similar.load_ratings(self.ratings_file.name)
+        self.similar.normalize_data()
 
-    def it_should_normaliza_the_ratings(self):
-        users = {'1', '2', '3', '12'}
-        movies = {'1193', '1357', '3421'}
-        ratings = {('1','1193'): 5, ('2', '1357'): 5, ('3', '3421'): 4, ('12', '1193'): 4}
-        normalized_ratings = normalize_data(users, movies, ratings)
-        normalized_ratings |should| equal_to({('1', '1193'): round(10.0/3, 3), \
+    def tearDown(self):
+        del self.similar
+
+    def it_should_load_user_ratings(self):
+        self.similar.users |should| equal_to({'1', '2', '3', '12'})
+        self.similar.movies |should| equal_to({'1193', '1357', '3421'})
+        self.similar.ratings |should| equal_to({('1','1193'): 5, ('2', '1357'): 5, \
+                                                ('3', '3421'): 4, ('12', '1193'): 4})
+
+    def it_should_normalize_the_ratings(self):
+        self.similar.utility_matrix |should| equal_to({('1', '1193'): round(10.0/3, 3), \
                                               ('2', '1357'): round(10.0/3, 3), \
                                               ('3', '3421'): round(8.0/3, 3), \
                                               ('12', '1193'): round(8.0/3, 3)})
+
+    def it_should_generate_ratings_for_each_user(self):
+        result = self.similar._get_ratings('1')
+        result |should| equal_to([round(10.0/3, 3), 0, 0])
+        result = self.similar._get_ratings('2')
+        result |should| equal_to([0, round(10.0/3, 3), 0])
+        result = self.similar._get_ratings('3')
+        result |should| equal_to([0, 0, round(8.0/3, 3)])
+        result = self.similar._get_ratings('12')
+        result |should| equal_to([round(8.0/3, 3), 0, 0])
+
+
+    def it_should_return_the_k_more_similar_users(self):
+        similar_users = self.similar.get_k_more_similar_users(2)
+        similar_users |should| equal_to(['1', '12'])
