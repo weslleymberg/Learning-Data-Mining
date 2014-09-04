874f8182edcd15c8c315bc6309605e4d48f70d00
Weslleymberg <weslleym.lisboa@gmail.com>
2014-09-04 18:17:58 -0300

    Calculates movie's rating average

--- content_recommendation/content_recommendation.py
+++ content_recommendation/content_recommendation.py
@@ -16,14 +16,19 @@ def sim(a, b):
     vdd = sqrt(sum_squared(a))*sqrt(sum_squared(b))
     return vd/vdd
 
+def jaccardsim(a, b):
+    return round(len(a.intersection(b))/float(len(a.union(b))), 3)
+
 
 class Similar(object):
     def __init__(self):
         self.users = set()
         self.movies = set()
+        self.movie_genres = {}
         self.ratings = {}
         self.utility_matrix = {}
         self.average_rate_by_user = {}
+        self.average_rate_by_movie = {}
 
     def load_ratings(self, file_name):
         for line in load_from_file(file_name):
@@ -32,23 +37,39 @@ class Similar(object):
             self.movies.add(movie_id)
             self.ratings[(user_id, movie_id)] = int(rating)
 
+    def load_movies(self, file_name):
+        for line in load_from_file(file_name):
+            movie_id, _, genre = line.strip().split("::")
+            self.movie_genres[movie_id] = set(genre.split("|"))
+
     def normalize_data(self):
-        scores = defaultdict(lambda: 0)
+        user_scores = defaultdict(lambda: 0)
+        movie_scores = defaultdict(lambda: 0)
         for i in product(self.users, self.movies):
             u_id, m_id = i
             try:
-                scores[u_id] += self.ratings[i]
+                rating = self.ratings[i]
+                user_scores[u_id] += rating
+                movie_scores[m_id] += rating
             except KeyError:
-                scores[u_id] += 0
+                user_scores[u_id] += 0
+                movie_scores[m_id] += 0
         for key, value in self.ratings.iteritems():
-            user_id, _ = key
-            avg = (float(scores[user_id])/len(self.movies))
-            self.utility_matrix[key] = round(value - avg, 3)
-            self.average_rate_by_user[user_id] = round(avg, 3)
+            user_id, movie_id = key
+            user_avg = (float(user_scores[user_id])/len(self.movies))
+            movie_avg = (float(movie_scores[movie_id])/len(self.users))
+            self.utility_matrix[key] = round(value - user_avg, 3)
+            self.average_rate_by_user[user_id] = round(user_avg, 3)
+            self.average_rate_by_movie[movie_id] = round(movie_avg, 3)
 
-    def _get_ratings(self, user_id):
+    def _get_ratings(self, id_, movie=False):
         user_ratings = []
-        for i in product((user_id,), self.movies):
+        if movie:
+            sequence = list(product((id_, ), self.users))
+        else:
+            sequence = product((id_, ), self.movies)
+        for i in sequence:
+            if movie: i = i[::-1]
             try:
                 rate = self.utility_matrix[i]
             except KeyError:
@@ -71,11 +92,32 @@ class Similar(object):
                 more_similars.add(i)
         return list(more_similars)
 
-    def calculate_average_rating(self, users):
+    def get_k_more_similar_movies(self, k):
+        similars = []
+        more_similars = set()
+        for a, b in combinations(self.movies, 2):
+            ratings_a = self._get_ratings(a, movie=True)
+            ratings_b = self._get_ratings(b, movie=True)
+            similarity = sim(ratings_a, ratings_b)
+            jaccard_similarity = jaccardsim(self.movie_genres[a], self.movie_genres[b])
+            similars.append(((a, b), (similarity+jaccard_similarity)/2))
+        sorted_similars = sorted(similars, key=lambda x: x[1], reverse=True)
+        for movie_pair, _ in sorted_similars:
+            for i in movie_pair:
+                if len(more_similars) == k:
+                    break
+                more_similars.add(i)
+        return list(more_similars)
+
+    def calculate_average_rating(self, sequence, movie=False):
         total = 0
-        for i in users:
-            total += self.average_rate_by_user[i]
-        return float(total)/len(users)
+        if movie:
+            dict_ = self.average_rate_by_movie
+        else:
+            dict_ = self.average_rate_by_user
+        for i in sequence:
+            total += dict_[i]
+        return float(total)/len(sequence)
 
 
 if __name__ == '__main__':
@@ -84,11 +126,21 @@ if __name__ == '__main__':
     similar = Similar()
     print 'Loading data from file...'
     similar.load_ratings('ratings_sample.dat')
+    similar.load_movies('movies.dat')
     print 'Normalazing data...'
     similar.normalize_data()
     print '>>Done initial processing in ', datetime.now()-startTime
-    print 'Calculating similarities...'
+    print "================================================="
+
+    print 'Calculating users similarities...'
     more_similar_users = similar.get_k_more_similar_users(3)
     avg = similar.calculate_average_rating(more_similar_users)
     print 'Average rating for the %i most similar users: %.5f' %(len(more_similar_users), avg)
-    print '>>Finished running in ', datetime.now()-startTime
+    print '>>Finished in: ', datetime.now()-startTime
+    print "================================================="
+
+    print 'Calculating movies similarities...'
+    more_similar_movies = similar.get_k_more_similar_movies(3)
+    avg = similar.calculate_average_rating(more_similar_movies, movie=True)
+    print 'Average rating for the %i most similar movies: %.5f' %(len(more_similar_movies), avg)
+    print '>>Finished in: ', datetime.now()-startTime
--- content_recommendation/content_recommendation_spec.py
+++ content_recommendation/content_recommendation_spec.py
@@ -3,7 +3,7 @@ import os
 from tempfile import NamedTemporaryFile
 from should_dsl import should
 
-from content_recommendation import Similar, sim
+from content_recommendation import Similar, sim, jaccardsim
 
 def generate_temporary_file(data):
     temp_file = NamedTemporaryFile(delete=False)
@@ -12,13 +12,18 @@ def generate_temporary_file(data):
     return temp_file
 
 
-class CosseneSimilarityTest(unittest.TestCase):
+class SimilarityFunctionsTest(unittest.TestCase):
 
     def it_should_return_the_similarity_between_two_sets(self):
         result = sim([5,0,0], [0,5,0])
         result |should| equal_to(0)
         result = sim([5,0,0], [4,0,0])
         result |should| equal_to(1)
+
+    def it_should_return_the_jaccard_similarity_between_two_sets(self):
+        result = jaccardsim({'Drama', 'Aventura'}, {'Aventura', 'Romance'})
+        result |should| equal_to(round(1.0/3, 3))
+
 
 
 class ContentRecommendationTest(unittest.TestCase):
@@ -27,8 +32,14 @@ class ContentRecommendationTest(unittest.TestCase):
     def setUpClass(cls):
         cls.ratings_file = generate_temporary_file("1::1193::5::978300760\n \
                                                     2::1357::5::978298709\n \
+                                                    2::2268::5::978299297\n \
                                                     3::3421::4::978298147\n \
                                                     12::1193::4::978220179")
+
+        cls.movies_file = generate_temporary_file("1193::One Flew Over the Cuckoo's Nest (1975)::Drama\n \
+                                                   1357::Shine (1996)::Drama|Romance\n \
+                                                   2268::Few Good Men, A (1992)::Crime|Drama\n \
+                                                   3421::Animal House (1978)::Comedy")
 
     @classmethod
     def tearDownClass(cls):
@@ -44,33 +55,47 @@ class ContentRecommendationTest(unittest.TestCase):
 
     def it_should_load_user_ratings(self):
         self.similar.users |should| equal_to({'1', '2', '3', '12'})
-        self.similar.movies |should| equal_to({'1193', '1357', '3421'})
+        self.similar.movies |should| equal_to({'1193', '1357', '3421', '2268'})
         self.similar.ratings |should| equal_to({('1','1193'): 5, ('2', '1357'): 5, \
-                                                ('3', '3421'): 4, ('12', '1193'): 4})
+                                                ('3', '3421'): 4, ('12', '1193'): 4, \
+                                                ('2', '2268'): 5})
 
     def it_should_normalize_the_ratings(self):
-        self.similar.utility_matrix |should| equal_to({('1', '1193'): round(10.0/3, 3), \
-                                              ('2', '1357'): round(10.0/3, 3), \
-                                              ('3', '3421'): round(8.0/3, 3), \
-                                              ('12', '1193'): round(8.0/3, 3)})
+        self.similar.utility_matrix |should| equal_to({('1', '1193'): round(15.0/4, 3), \
+                                              ('2', '1357'): round(10.0/4, 3), \
+                                              ('2', '2268'): round(10.0/4, 3), \
+                                              ('3', '3421'): round(3.0, 3), \
+                                              ('12', '1193'): round(3.0, 3)})
 
     def it_should_generate_ratings_for_each_user(self):
         result = self.similar._get_ratings('1')
-        result |should| equal_to([round(10.0/3, 3), 0, 0])
+        result |should| equal_to([0, round(15.0/4, 3), 0, 0])
         result = self.similar._get_ratings('2')
-        result |should| equal_to([0, round(10.0/3, 3), 0])
+        result |should| equal_to([0, 0, round(10.0/4, 3), round(10.0/4, 3)])
         result = self.similar._get_ratings('3')
-        result |should| equal_to([0, 0, round(8.0/3, 3)])
+        result |should| equal_to([round(3.0, 3), 0, 0, 0])
         result = self.similar._get_ratings('12')
-        result |should| equal_to([round(8.0/3, 3), 0, 0])
+        result |should| equal_to([0, round(3.0, 3), 0, 0])
 
     def it_should_keep_track_of_users_average_rate(self):
-        self.similar.average_rate_by_user |should| equal_to({'1': round(5.0/3, 3), \
-                                                             '2': round(5.0/3, 3), \
-                                                             '3': round(4.0/3, 3), \
-                                                             '12': round(4.0/3, 3)})
+        self.similar.average_rate_by_user |should| equal_to({'1': round(5.0/4, 3), \
+                                                             '2': round(10.0/4, 3), \
+                                                             '3': round(4.0/4, 3), \
+                                                             '12': round(4.0/4, 3)})
 
 
     def it_should_return_the_k_more_similar_users(self):
         similar_users = self.similar.get_k_more_similar_users(2)
         similar_users |should| equal_to(['1', '12'])
+
+    def it_should_load_the_movies_file(self):
+        self.similar.load_movies(self.movies_file.name)
+        self.similar.movie_genres |should| equal_to({'1193':{'Drama'}, \
+                                                     '1357':{'Drama', 'Romance'}, \
+                                                     '2268':{'Crime', 'Drama'}, \
+                                                     '3421':{'Comedy'}})
+
+    def it_should_return_the_k_more_similar_movies(self):
+        self.similar.load_movies(self.movies_file.name)
+        similar_movies = self.similar.get_k_more_similar_movies(2)
+        similar_movies |should| equal_to(['2268', '1357'])
